1ae608f9880cde2ffb7a9eb40e9388c469a2b52f
iyunx1 <lalalalala12214@gmail.com>
2026-05-12 13:15:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기')
 st.text_tnput('이름을 입력해주세요')
-st.selectbox('좋아하는 음식을 입력해주세요!!',['마라탕','닭발','덕볶이'])
+st.selectbox('좋아하는 음식을 입력해주세요!!',['마라탕','닭발','떡볶이'])
 st.boutton('인사말 생성')
